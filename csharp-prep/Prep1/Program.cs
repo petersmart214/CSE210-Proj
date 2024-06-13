210efab39d43b21ebed12db4d93ba7fd52e172a9
SunShine214 <sunshineprojects214@gmail.com>
2024-06-12 23:29:15 -0600

    Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Loop();   
     }
+    static void Loop() {
+        Console.WriteLine("ID, please...");
+        string[] name = Console.ReadLine().Split(' ');
+        if (name.Length != 2) Loop(); else {
+            Console.WriteLine($"Writing for you...\nHello, {name[0]}, {name[0]} {name[1]}");
+        }
+        
+    }
+
 }
