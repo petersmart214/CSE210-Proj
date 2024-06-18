cb43a6908ca7c14822e924752195b403c0eefbba
SunShine214 <sunshineprojects214@gmail.com>
2024-06-18 14:02:33 -0600

    Prep3 done (Lateeeeeeeeeeeeeeee)

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Linq.Expressions;
 
 class Program
 {
+
+    public static void Loop() {
+        Console.WriteLine("Input a Random Number 1 to 10");
+        Boolean cont = true;
+        int randNum = new Random().Next(1, 11);
+        while (cont) {
+            int picked = int.Parse(Console.ReadLine());
+            if (randNum == picked) {Console.WriteLine("You got it!"); break;}
+            if (randNum > picked) Console.WriteLine("Higher");
+            if (randNum < picked) Console.WriteLine("Lower");
+        }
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Loop();
     }
 }
