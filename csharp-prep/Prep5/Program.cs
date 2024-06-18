c705896ca419d4db3797ac9d1d150d154774e7d2
SunShine214 <sunshineprojects214@gmail.com>
2024-06-18 14:29:51 -0600

    Prep5!!!!!

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,30 @@ using System;
 
 class Program
 {
+
+    public static void DisplayWelcome() {
+        Console.WriteLine("Welcome to the Program");
+    }
+
+    public static string PromptUserName() {
+        Console.WriteLine("Please input your name...");
+        return Console.ReadLine();
+    }
+    public static int PromptUserNumber() {
+        Console.WriteLine("Please input your number...");
+        return int.Parse(Console.ReadLine());
+    }
+
+    public static int SquareNumber(int numToSquare) {
+        return (int) Math.Sqrt(numToSquare);
+    }
+
+    public static void DisplayResult(string userName, int userNum) {
+        Console.WriteLine($"For {userName} your number is {userNum}.");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        DisplayResult(PromptUserName(), SquareNumber(PromptUserNumber()));
     }
 }
